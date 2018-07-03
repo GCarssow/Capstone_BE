98871100fc6fd3e05018b1425b8dc50b2fd3ecb7
Graham <gcarssow@gmail.com>
2018-07-03 10:15:32 -0600

    Backup

--- seeds/01_equipment.js
+++ seeds/01_equipment.js
@@ -7,9 +7,9 @@ exports.seed = function(knex, Promise) {
           {
             id: 1,
             park_name: "Wash Park",
-            equipment_available_1: "pull-up bars",
-            equipment_available_2: "park bench",
-            equipment_available_3: "playground with jungle gym",
+            equipment_available_1: "Pull-up bars",
+            equipment_available_2: "Park bench",
+            equipment_available_3: "Playground with jungle gym",
             equipment_available_4: "Running Trails",
             equipment_available_5: "",
             equipment_available_6: "",
@@ -22,8 +22,8 @@ exports.seed = function(knex, Promise) {
             {
             id: 2,
             park_name: "Prairie Meadows Park",
-            equipment_available_1: "park bench",
-            equipment_available_2: "jungle gym",
+            equipment_available_1: "Park bench",
+            equipment_available_2: "Jungle gym",
             equipment_available_3: "Running trails",
             equipment_available_4: "",
             equipment_available_5: "",
@@ -33,8 +33,37 @@ exports.seed = function(knex, Promise) {
             equipment_available_9: "",
             equipment_available_10: "",
             source: "Graham Carssow"
-            },  
-
-        ]).then(() => knex.raw('ALTER SEQUENCE equipment_id_seq RESTART WITH 3;'));
+            },
+            {
+            id: 3,  
+            park_name: "Prairie Meadows Park",
+            equipment_available_1: "Park bench",
+            equipment_available_2: "Jungle gym",
+            equipment_available_3: "Running trails",
+            equipment_available_4: "Climbing Wall",
+            equipment_available_5: "",
+            equipment_available_6: "",
+            equipment_available_7: "",
+            equipment_available_8: "",
+            equipment_available_9: "",
+            equipment_available_10: "",
+            source: "Graham Carssow"
+            },
+            {
+              id: 4,  
+              park_name: "Central Park",
+              equipment_available_1: "Park bench",
+              equipment_available_2: "Jungle gym",
+              equipment_available_3: "Running trails",
+              equipment_available_4: "Hills for Sprints",
+              equipment_available_5: "Soccer Field",
+              equipment_available_6: "",
+              equipment_available_7: "",
+              equipment_available_8: "",
+              equipment_available_9: "",
+              equipment_available_10: "",
+              source: "Graham Carssow"
+              }
+        ]).then(() => knex.raw('ALTER SEQUENCE equipment_id_seq RESTART WITH 5;'));
       });
   };
